d527093fbd3f1c3c347606cf05faae0e1576f9dc
Melis Peker <melis.peker@hisarschool.k12.tr>
2024-12-15 22:18:50 +0300

    Initial Commit

---
+++ SwiftUI Calc/SwiftUI_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalcApp.swift
+//  SwiftUI Calc
+//
+//  Created by Melis Peker on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
